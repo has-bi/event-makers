927ccf739ddde9d149fcfc440af51d8eaa3b721c
hassadiqin <hassadiqin.31@gmail.com>
2025-02-23 00:18:19 +0700

    Revert "create a landing page and adding heroUI and reconfigure TailwindCSS"

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,17 @@
-const { heroui } = require("@heroui/theme");
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/button.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
-  darkMode: "class",
-  plugins: [heroui()],
+  plugins: [],
 };
