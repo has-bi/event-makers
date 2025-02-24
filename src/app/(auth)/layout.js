c2152488be4ac9b8c1583835653c04627486a594
taufiqrohmand <taufiqrohmand@gmail.com>
2025-02-24 12:13:38 +0700

    styling

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Layout({ children }) {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="w-[300px] bg-slate-50 rounded-lg">{children}</div>
+      <div className="w-[300px] bg-slate-50 rounded-lg border-slate-300">
+        {children}
+      </div>
     </div>
   );
 }
